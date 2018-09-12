daea53ae65ea2491429b2568a7ce5cd222c0d70b
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2018-09-12 15:43:23 +0300

    Initial

---
+++ 12.09 app/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._09_app
+{
+    class Program
+    {
+        public static DeviceManager _manager = new DeviceManager();
+
+        static void Main(string[] args)
+        {
+            DeviceManager.Instance.SendData();
+            //var dm1 = new DeviceManager();
+            //var dm2 = new DeviceManager();
+        }
+    }
+    public class DeviceManager
+    {
+        private static DeviceManager _managerInstance; // = new DeviceManager();
+        private static Lazy<DeviceManager> _managerInstanceLazy;
+         //_managerInstance = new DeviceManager();
+        static DeviceManager()
+        {
+            _managerInstanceLazy = new Lazy<DeviceManager>(() =>
+            {
+                return new DeviceManager();
+            });
+
+        }
+        public static DeviceManager Instance
+        {
+            get
+            {
+                if (_managerInstance == null) 
+                {
+                    _managerInstance = new DeviceManager();
+                }
+                {
+                    return _managerInstance;
+                }
+            }
+        }
+        public DeviceManager GetInstance()
+        {
+            return _managerInstance;
+        }
+        public void SendData()
+        {
+
+        }
+    }
+}
